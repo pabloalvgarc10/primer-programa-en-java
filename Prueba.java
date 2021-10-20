7e0bdbc58c51b661881a731149f72a14931017db
pabloalvgarc10 <92783613+pabloalvgarc10@users.noreply.github.com>
2021-10-20 10:41:49 +0200

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -1,11 +1,10 @@
 public class Prueba {
 
     public static void main(String args[]) {
-
-        System.out.println("Pablo Alvarez");
+      
         System.out.println("Leon 2001");
         System.out.println("Leon Windows 10");
-
+        System.out.println("RVFV)";
      }
 
 }
